f0e81f90df9a7a7ed708861d9eda494aee315b6b
liangke <lokyse@163.com>
2021-03-25 23:36:21 +0800

    修改

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+ 
+app.use('/', express.static('dist'));
+ 
+app.get('/', function (req, res) {
+//    res.send('Hello World');
+    res.sendFile( __dirname + "/dist/" + "index.html" );
+})
+ 
+var server = app.listen(9001, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+ 
+})
